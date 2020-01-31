5fc3a1a912aefe8f9391c1151ec3d53ac6ff60f5
niswonger <54144302+niswonger@users.noreply.github.com>
2020-01-31 16:23:30 -0500

    Added an analysis for university majors

--- lme_main.R
+++ lme_main.R
@@ -15,10 +15,7 @@ dt.census <- readRDS('Data/ACS/census.RDS')[YEAR %in% 2009:2018]
 dt.year_bins <-data.table(YEAR = 2009:2018,group = sort(rep(1:2,5)),rank = rep(1:5,2))
 
 dt.census <- merge(dt.census, dt.year_bins , by = 'YEAR')
-# Define list of stem majors based on "DHS determined a “STEM field” is a field included in the 
-# Department of Education’s CIP taxonomy within the two-digit series containing engineering (CIP code 14), 
-# biological sciences (CIP code 26), mathematics (CIP code 27), physical sciences (CIP code 40) or a 
-# related field." 
+# Define list of stem majors based on "DHS determined a “STEM field” is a field included in the Department of Education’s CIP taxonomy within the two-digit series containing engineering (CIP code 14), biological sciences (CIP code 26), mathematics (CIP code 27), physical sciences (CIP code 40) or a related field." 
 # For the DEGFIELD codes this implies the following
 dt.census[,ed_type := ifelse(EDUC<7, "no_college",
                              ifelse(DEGFIELD==0,'no_deg', 
@@ -31,6 +28,18 @@ dt.wage_growth <- dt.census[WKSWORK2 >= 5 & EMPSTAT == 1 & AGE %in% 25:55 ,
                             by = list(STATEFIP,ed_type,group)]
 dt.wage_growth_overall <- dt.census[WKSWORK2 >= 5 & EMPSTAT == 1 & AGE %in% 25:55 ,list(wage_growth_state = (.SD[rank == 5, mean(INCWAGE)]-.SD[rank == 1, mean(INCWAGE)])/.SD[rank == 1, mean(INCWAGE)]),
                                     by = list(STATEFIP,group)]
+# For looking at wage distributions
+# dt.mean_wage <- dt.census[WKSWORK2 >= 5 & EMPSTAT == 1 & AGE %in% 25:55 & group ==1 & STATEFIP != 11,
+#                           list(mean_wage = sum(PERWT*INCWAGE)/sum(PERWT)),
+#                                     by = list(STATEFIP)]
+# dt.mean_wage[mean_wage == max(mean_wage)]
+# dt.mean_wage[mean_wage == min(mean_wage)]
+# ggplot(dt.census[WKSWORK2 >= 5 & EMPSTAT == 1 & AGE %in% 25:55 & group ==1& STATEFIP %in% c(30)],
+#        aes(x = INCWAGE, fill = factor(STATEFIP)))+
+#   geom_histogram(aes(y=..count../sum(..count..), fill = factor(STATEFIP)), position = 'dodge',fill = "blue", alpha = 0.2) +
+#   geom_histogram(data = dt.census[WKSWORK2 >= 5 & EMPSTAT == 1 & AGE %in% 25:55 & group ==1& STATEFIP %in% c(34)], 
+#                  aes(x = INCWAGE,y=..count../sum(..count..), fill = factor(STATEFIP)), position = 'dodge',fill = "red", alpha = 0.2)
+  
 # Stagger group for merging later
 dt.wage_growth[,group := group +1]
 dt.wage_growth[,frac_emp := emp/sum(emp),by = list(STATEFIP,group)]
@@ -73,4 +82,64 @@ model <- glm(STEM ~wage_growth_STEM_std*frac_emp_STEM_std + wage_growth_state + 
 summary(model)
 # Export to STATA for tables
 write.dta(dt.marjor_wage, "Data/Cleaned_Data/regressions.dta")
- 
+#####################################################################################################################
+#####################################################################################################################
+# Try similar analysis from the college side
+dt.major <- readRDS('~/Documents/Harvard/Research/College_Response/Data/College_Info/IPEDS_completion_tables/ipeds_cat.rds')
+# Get corresponding years and only foucs on all degrees granted
+dt.major <- dt.major[year %in% 2009:2018 & cat == 'total' & level == 5]
+dt.major <- merge(dt.year_bins[,list(year= YEAR, group, rank)],dt.major,by = 'year')
+# Group STEM related degrees
+dt.major[,STEM := ifelse(cip %in% c(14, 15, 26, 27,40, 41),'STEM','not_STEM')]
+# Collapse to total over the period for STEM degrees
+dt.major <- dt.major[,list(num = sum(num)) , by = list(unitid,STEM,group)]
+# Get address of colleges 
+dt.college_address <- readRDS("~/Documents/Harvard/Research/College_Response/Data/College_Info/IPEDS_IC/ic.rds")
+# Pull in the zip to county crosswalk
+dt.cty_zip <- data.table(read.csv('~/Documents/Harvard/Research/College_Response/Data/Geography/geocorr_zip_county_mapping.csv'))
+dt.cty_zip <- dt.cty_zip[,list(ZIP = zcta5, county,STUSAB = stab  )]
+# merge to get the county for the college
+dt.college_cty <- merge(dt.college_address[,list(UNITID,ZIP = as.numeric(ZIP))],dt.cty_zip, by = 'ZIP' )
+dt.maj_cty <- merge(dt.college_cty[,list(unitid = as.numeric(UNITID), county, STUSAB)], 
+                          dt.major[,list(unitid = as.numeric(unitid), num,STEM,group)], by = c('unitid'), allow.cartesian = T)
+# First account for the fact that some zip codes are mapped to multiple counties
+dt.maj_cty[,cty_count := .N, by = list(unitid,STEM,group)]
+dt.maj_cty[, num := num/cty_count]
+# Aggregate to county level enrollment
+dt.maj_cty <- dt.maj_cty[,list(num = sum(num)), by = list(county, STUSAB,STEM,group)]
+# Get FIP information
+dt.state <- data.table(read.csv('~/Documents/Harvard/Research/College_Response/Data/Geography/state_fips.csv'))
+dt.maj_cty <- merge(dt.maj_cty, dt.state[,list(fipstate = Fips, STUSAB, state = STATE_NAME)], by = 'STUSAB')
+dt.maj_cty[,fipscty := as.numeric(substr(county,nchar(county)-2 , nchar(county)))]
+#### Now we have the STEM majors by county for two time periods ####
+# Incorporate county level census data. 
+dt.wage_growth_cty <- dt.census[WKSWORK2 >= 5 & EMPSTAT == 1 & AGE %in% 25:55 ,
+                            list(wage_growth = (.SD[rank == 5, mean(INCWAGE)]-.SD[rank == 1, mean(INCWAGE)])/.SD[rank == 1, mean(INCWAGE)],
+                                 mean_wage = sum(PERWT*INCWAGE)/sum(PERWT),
+                                 emp = sum(PERWT)),
+                            by = list(STATEFIP, COUNTYFIP, ed_type,group)]
+dt.wage_growth_overall_cty <- dt.census[WKSWORK2 >= 5 & EMPSTAT == 1 & AGE %in% 25:55 ,list(wage_growth_county = (.SD[rank == 5, mean(INCWAGE)]-.SD[rank == 1, mean(INCWAGE)])/.SD[rank == 1, mean(INCWAGE)]),
+                                    by = list(STATEFIP,COUNTYFIP, group)]
+dt.wage_growth_cty[,group := group+1]
+dt.wage_growth_cty[,frac_emp := emp/sum(emp),by = list(STATEFIP,group)]
+dt.wage_growth_cty <- dcast(dt.wage_growth_cty, STATEFIP + group +  COUNTYFIP~ ed_type, value.var = c('wage_growth','frac_emp','mean_wage'))
+dt.wage_growth_cty[,STEM_premium := mean_wage_STEM/mean_wage_not_STEM]
+
+
+dt.major_reg <- merge(dt.maj_cty[,list(STEM,group,num,STATEFIP = fipstate, COUNTYFIP = fipscty)],
+                      dt.wage_growth_cty,by = c('STATEFIP','COUNTYFIP','group'))
+# merge total wage growth
+dt.major_reg <- merge(dt.wage_growth_overall_cty[,list(STATEFIP,group,COUNTYFIP,wage_growth_county)],
+                      dt.major_reg, by = c('STATEFIP','COUNTYFIP','group'))
+dt.major_reg[,total:= sum(num), by = list(STATEFIP,COUNTYFIP,group)]
+dt.major_reg[STEM == 'STEM',frac_stem:= num/total, by = list(STATEFIP,COUNTYFIP,group)]
+dt.major_reg[STEM == 'STEM',log_stem:= log(num)]
+dt.major_reg[STEM == 'STEM',log_total:= log(total)]
+# Normalize
+dt.major_reg[!is.na(wage_growth_STEM),wage_growth_STEM_std := (wage_growth_STEM-mean(wage_growth_STEM))/sd(wage_growth_STEM)]
+dt.major_reg[,frac_emp_STEM_std := (frac_emp_STEM-mean(frac_emp_STEM))/sd(frac_emp_STEM)]
+dt.major_reg[,STEM_premium_std := (STEM_premium-mean(STEM_premium))/sd(STEM_premium)]
+# Run logit on likelihood of STEM degree
+model <- lm(log_stem ~ wage_growth_STEM_std*frac_emp_STEM_std + wage_growth_county + STEM_premium_std*frac_emp_STEM_std + log_total+ factor(STATEFIP),
+            dt.major_reg[STEM == 'STEM'& num > 0])
+summary(model)
